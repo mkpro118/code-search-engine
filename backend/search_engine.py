19979547b432dd05b583d9ba7cfef85d3401555b
M K <mkpro118@gmail.com>
2024-06-24 17:03:05 -0500

    Template engine (WIP)

---
+++ backend/search_engine.py
@@ -0,0 +1,7 @@
+import dataclasses
+from typing import Optional
+
+from snippets.lancedb.config import DBConfig, ST_Config
+from snippets.lancedb.factory import SnippetSchemaFactory
+from snippets.lancedb.table import SnippetTable
+from snippets.lancedb.generator import SnippetGenerator
